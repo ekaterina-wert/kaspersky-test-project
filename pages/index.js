75ccdb8a6ac762dbb6bf708ea16e9a93d4ef7b05
unknown <euveret@yandex.ru>
2021-09-08 00:48:23 +0500

    feat: sticky blocks

--- pages/index.js
+++ pages/index.js
@@ -11,67 +11,141 @@ const newPriceBlock = document.querySelector('.buy-block__price_new');
 const oldPriceBlock = document.querySelector('.buy-block__price_old');
 
 const buyNowButton = document.querySelector('.buy-block__submit');
+const stickyBuyNowButton = document.querySelector('.sticky-buy-block__submit');
 
-// tariffs object.
+const header = document.querySelector('.v10__header');
+
+const buyBlock = document.querySelector('.buy-block');
+const stickyBuyBlock = document.querySelector('.sticky-buy-block');
+const stickyTariff = document.querySelector('.sticky-buy-block__open-full');
+const reachedBlock = buyBlock.offsetTop;
+
+let scroll = 0;
+
+// tariffs object
 const tariff = {
     GBP: {
         currency: '£',
-        price: {
-            oneDevOneYr: 27,
-            oneDevTwoYr: 43,
-            threeDevOneYr: 35,
-            threeDevTwoYr: 63,
-            fiveDevOneYr: 43,
-            fiveDevTwoYr: 71,
-        },
-        name: {
-            oneDevOneYr: '1 Device, 1 Year',
-            oneDevTwoYr: '1 Device, 2 Years',
-            threeDevOneYr: '3 Devices, 1 Year',
-            threeDevTwoYr: '3 Devices, 2 Years',
-            fiveDevOneYr: '5 Devices, 1 Year',
-            fiveDevTwoYr: '5 Devices, 2 Years',
+        plan: {
+            oneDevOneYr: {
+                price: 27,
+                name: '1 Device, 1 Year'
+            },
+            oneDevTwoYr: {
+                price: 43,
+                name: '1 Device, 2 Years'
+            },
+            threeDevOneYr: {
+                price: 35,
+                name: '3 Devices, 1 Year'
+            },
+            threeDevTwoYr: {
+                price: 63,
+                name: '3 Devices, 2 Years'
+            },
+            fiveDevOneYr: {
+                price: 43,
+                name: '5 Devices, 1 Year'
+            },
+            fiveDevTwoYr: {
+                price: 71,
+                name: '5 Devices, 2 Years'
+            },
         }
     },
     EUR: {
         currency: '€',
-        price: {
-            oneDevOneYr: 27,
-            oneDevTwoYr: 43,
-            threeDevOneYr: 35,
-            threeDevTwoYr: 63,
-            fiveDevOneYr: 43,
-            fiveDevTwoYr: 71,
-        },
-        name: {
-            oneDevOneYr: '1 Device, 1 Year',
-            oneDevTwoYr: '1 Device, 2 Years',
-            threeDevOneYr: '3 Devices, 1 Year',
-            threeDevTwoYr: '3 Devices, 2 Years',
-            fiveDevOneYr: '5 Devices, 1 Year',
-            fiveDevTwoYr: '5 Devices, 2 Years',
+        plan: {
+            oneDevOneYr: {
+                price: 27,
+                name: '1 Device, 1 Year'
+            },
+            oneDevTwoYr: {
+                price: 43,
+                name: '1 Device, 2 Years'
+            },
+            threeDevOneYr: {
+                price: 35,
+                name: '3 Devices, 1 Year'
+            },
+            threeDevTwoYr: {
+                price: 63,
+                name: '3 Devices, 2 Years'
+            },
+            fiveDevOneYr: {
+                price: 43,
+                name: '5 Devices, 1 Year'
+            },
+            fiveDevTwoYr: {
+                price: 71,
+                name: '5 Devices, 2 Years'
+            },
         }
     },
     USD: {
         currency: '$',
-        price: {
-            oneDevOneYr: 27,
-            oneDevTwoYr: 43,
-            threeDevOneYr: 35,
-            threeDevTwoYr: 63,
-            fiveDevOneYr: 43,
-            fiveDevTwoYr: 71,
-        },
-        name: {
-            oneDevOneYr: '1 Device, 1 Year',
-            oneDevTwoYr: '1 Device, 2 Years',
-            threeDevOneYr: '3 Devices, 1 Year',
-            threeDevTwoYr: '3 Devices, 2 Years',
-            fiveDevOneYr: '5 Devices, 1 Year',
-            fiveDevTwoYr: '5 Devices, 2 Years',
+        plan: {
+            oneDevOneYr: {
+                price: 27,
+                name: '1 Device, 1 Year'
+            },
+            oneDevTwoYr: {
+                price: 43,
+                name: '1 Device, 2 Years'
+            },
+            threeDevOneYr: {
+                price: 35,
+                name: '3 Devices, 1 Year'
+            },
+            threeDevTwoYr: {
+                price: 63,
+                name: '3 Devices, 2 Years'
+            },
+            fiveDevOneYr: {
+                price: 43,
+                name: '5 Devices, 1 Year'
+            },
+            fiveDevTwoYr: {
+                price: 71,
+                name: '5 Devices, 2 Years'
+            },
         }
     }
 };
+
+// Buy-Block position according to scrollY
+function stickBlock() {
+    if (scrollY > reachedBlock) {
+
+        if (document.documentElement.clientWidth < 862) {
+            buyBlock.classList.add('buy-block_hidden');
+            stickyBuyBlock.classList.remove('sticky-buy-block_hidden')
+        } else {
+            buyBlock.classList.add('buy-block_fixed');
+        }
+
+    } else {
+        if (document.documentElement.clientWidth < 862) {
+            buyBlock.classList.remove('buy-block_hidden');
+            stickyBuyBlock.classList.add('sticky-buy-block_hidden')
+        } else {
+            buyBlock.classList.remove('buy-block_fixed');
+        }
+    }
+}
+
+// Show Header on backscroll
+function showHeader() {
+    let top = window.pageYOffset;
+
+    if (scroll > 300 && scroll > top) {
+        header.classList.add('v10__header_fixed');
+
+    } else if (scroll < top) {
+        header.classList.remove('v10__header_fixed');
+    }
+    scroll = top;
+}
 
 // helper for picking the correct block from tariffs object according to selected currency
 function getTariff() {
@@ -86,7 +160,7 @@ function handleDropdownOpen() {
     tariffDropdown.classList.toggle('buy-block__dropdown_hidden');
 
     tariffArr.forEach((item) => {
-        const option = `${tariffBlock.name[item.dataset.tariff]} ${tariffBlock.currency}${tariffBlock.price[item.dataset.tariff]}.99`;
+        const option = `${tariffBlock.plan[item.dataset.tariff].name} ${tariffBlock.currency}${tariffBlock.plan[item.dataset.tariff].price}.99`;
         item.textContent = option;
     })
 }
@@ -97,25 +171,32 @@ function handleSetCurrency() {
 
     const selectedTariff = document.querySelector('.buy-block__selected-item').dataset.tariff;
 
-    newPriceBlock.innerHTML = tariffBlock.currency + tariffBlock.price[selectedTariff] + '<span class="buy-block__price buy-block__price_new-cents">.99</span>';
-    oldPriceBlock.innerHTML = tariffBlock.currency + Math.trunc(tariffBlock.price[selectedTariff] * 1.2) + '<span class="buy-block__price buy-block__price_old-cents">.99</span>';
+    newPriceBlock.innerHTML = tariffBlock.currency + tariffBlock.plan[selectedTariff].price + '<span class="buy-block__price buy-block__price_new-cents">.99</span>';
+    oldPriceBlock.innerHTML = tariffBlock.currency + Math.trunc(tariffBlock.plan[selectedTariff].price * 1.2) + '<span class="buy-block__price buy-block__price_old-cents">.99</span>';
 }
 
 // Set selected tariff and price
 function handleSetTariff(item) {
     const tariffBlock = getTariff();
 
-    const option = tariffBlock.name[item.dataset.tariff];
+    const option = tariffBlock.plan[item.dataset.tariff].name;
     selectedTariff.textContent = option;
 
-    newPriceBlock.innerHTML = tariffBlock.currency + tariffBlock.price[item.dataset.tariff] + '<span class="buy-block__price buy-block__price_new-cents">.99</span>';
-    oldPriceBlock.innerHTML = tariffBlock.currency + Math.trunc(tariffBlock.price[item.dataset.tariff] * 1.2) + '<span class="buy-block__price buy-block__price_old-cents">.99</span>';
+    newPriceBlock.innerHTML = tariffBlock.currency + tariffBlock.plan[item.dataset.tariff].price + '<span class="buy-block__price buy-block__price_new-cents">.99</span>';
+    oldPriceBlock.innerHTML = tariffBlock.currency + Math.trunc(tariffBlock.plan[item.dataset.tariff].price * 1.2) + '<span class="buy-block__price buy-block__price_old-cents">.99</span>';
+
+    stickyTariff.textContent = `${tariffBlock.currency}${tariffBlock.plan[item.dataset.tariff].price}.99 ${option}`
 
     selectedTariff.setAttribute('data-tariff', `${item.dataset.tariff}`)
     tariffDropdown.classList.toggle('buy-block__dropdown_hidden');
 }
 
-// Submit
+function handleShowFullBuyBlock() {
+    buyBlock.classList.add('');
+    stickyBuyBlock.classList.add('sticky-buy-block_hidden')
+}
+
+// Submit for purchase
 function handleBuyNow(e) {
     e.preventDefault();
 
@@ -123,9 +204,14 @@ function handleBuyNow(e) {
 
     const selectedTariff = document.querySelector('.buy-block__selected-item').dataset.tariff;
 
-    alert(`You are going to pay ${tariffBlock.currency}${tariffBlock.price[selectedTariff]}.99. You will be redirected to the purchasing page soon.`)
+    alert(`You are going to pay ${tariffBlock.currency}${tariffBlock.plan[selectedTariff].price}.99. You will be redirected to the purchasing page soon.`)
 }
 
+// listener for scroll: Buy-Block actions and Header on backscroll
+window.addEventListener('scroll', () => {
+    stickBlock();
+    showHeader()
+});
 
 // listener for currency options
 currencyForm.addEventListener('change', () => {
@@ -142,5 +228,9 @@ tariffArr.forEach((item) => {
 // listener for tariffs dropdown
 selectedTariff.addEventListener('click', handleDropdownOpen);
 
+stickyTariff.addEventListener('click', handleShowFullBuyBlock);
+
+
 // listener for submit
 buyNowButton.addEventListener('click', handleBuyNow);
+stickyBuyNowButton.addEventListener('click', handleBuyNow);
